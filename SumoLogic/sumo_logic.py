a71c4092469ed9e057a5b36279ac7de8500f0f15
josev814 <josev814@gmail.com>
2020-10-22 10:57:33 -0400

    using from import to import packages in sumo_logic
    entering a memory profiler to see how memory is used as the program runs

--- SumoLogic/sumo_logic.py
+++ SumoLogic/sumo_logic.py
@@ -1,35 +1,43 @@
 #!/usr/bin/env python3
+# 9.08203125 MB
+import memory_profiler
+START_MEMORY = memory_profiler.memory_usage()
+
 # Built-in Libraries
-from os.path import join as ospj
-import os
-import sys
-import signal
-import time
+from os import getpid, fstat, stat
+from sys import exit, maxsize
+from signal import signal, SIGTERM
+from time import time, sleep
 from logging import INFO as LOG_INFO, DEBUG as LOG_DEBUG
-import socket
+from socket import gethostname
 from stat import ST_SIZE, ST_INO
+from platform import architecture
+
+arch = architecture()
+if len(arch) == 1 or not arch[1].startswith('Windows'):
+    from signal import SIGUSR1
 
 # Third Party Libraries
-import gzip
+from gzip import GzipFile
 try:
-    import bz2
+    from bz2 import BZ2File
     HAS_BZ2 = True
 except ImportError:
     HAS_BZ2 = False
     bz2 = None
 
 # Package Libraries
-from .constants import *
-from .log_message import LogMessage
-from .prefs import SumoConfig
-from .version import VERSION
-from .daemon import createdaemon
-from .file_tracker import FileTracker
-from .sync import Sync
-from .util import calculate_seconds
+from SumoLogic.constants import *
+from SumoLogic.log_message import LogMessage
+from SumoLogic.prefs import SumoConfig
+from SumoLogic.version import VERSION
+from SumoLogic.daemon import createdaemon
+from SumoLogic.file_tracker import FileTracker
+from SumoLogic.sync import Sync
+from SumoLogic.util import calculate_seconds
 
-HOSTNAME = socket.gethostname()
-
+# .5151625 MB
+HOSTNAME = gethostname()
 
 class SumoLogic(object):
 
@@ -42,16 +50,22 @@ class SumoLogic(object):
         self.__logfile = logfile
         self.sync_lines = []
         # set last sync to current time
-        self.sync = Sync(self.__prefs, time.time())
+        self.sync = Sync(self.__prefs, time())
 
         self.sumoconfig = SumoConfig(config, user_config)
         self.log_message = LogMessage('SumoLogic')
         self.__sync_interval = calculate_seconds(self.sumoconfig.get_value('default', 'sync_interval'))
         self.__daemon_sleep = calculate_seconds(self.sumoconfig.get_value('default', 'daemon_sleep'))
 
+        for mem_usage in MEMDIFFS:
+            self.log_message.send_message(
+                'info',
+                'Memory Usage for {} is {}'.format(mem_usage, MEMDIFFS[mem_usage])
+            )
+
         try:
             self.file_tracker = FileTracker(
-                ospj(self.sumoconfig.get_value('default', 'work_dir'), OFFSETS_DIR),
+                os.path.join(self.sumoconfig.get_value('default', 'work_dir'), OFFSETS_DIR),
                 logfile
             )
         except Exception as e:
@@ -113,7 +127,7 @@ class SumoLogic(object):
         )
         self.log_message.send_message(
             'debug',
-            'Received SIGTERM for {}'.format(os.getpid())
+            'Received SIGTERM for {}'.format(getpid())
         )
         self.log_message.send_message(
             'info',
@@ -124,7 +138,7 @@ class SumoLogic(object):
         self.__lock_file.remove()
         # lock will be freed on SIGTERM by sumologic.py
         # exception handler (SystemExit)
-        sys.exit(0)
+        exit(0)
 
     def toggledebug(self):
         level = self.log_message.get_level()
@@ -141,13 +155,14 @@ class SumoLogic(object):
         self.log_message.set_level(level)
 
     def rundaemon(self, logfile, last_offset):
+
         # signal.signal(signal.SIGHUP, self.killdaemon)
-        signal.signal(signal.SIGTERM, self.killdaemon)
-        signal.signal(signal.SIGUSR1, self.toggledebug)
+        signal(SIGTERM, self.killdaemon)
+        signal(SIGUSR1, self.toggledebug)
 
         self.log_message.send_message(
             'info',
-            'SumoLogic daemon is now running, pid: {}'.format(os.getpid())
+            'SumoLogic daemon is now running, pid: {}'.format(getpid())
         )
         self.log_message.send_message(
             'info',
@@ -155,7 +170,7 @@ class SumoLogic(object):
         )
         self.log_message.send_message(
             'info',
-            '  eg.  kill -TERM {}'.format(os.getpid())
+            '  eg.  kill -TERM {}'.format(getpid())
         )
         self.__lock_file.create()
 
@@ -167,24 +182,24 @@ class SumoLogic(object):
 
     def daemonloop(self, logfile, last_offset):
         fp = open(logfile['log_file'], "r")
-        inode = os.fstat(fp.fileno())[ST_INO]
+        inode = fstat(fp.fileno())[ST_INO]
 
         while True:
             try:
-                curr_inode = os.stat(logfile['log_file'])[ST_INO]
+                curr_inode = stat(logfile['log_file'])[ST_INO]
             except OSError:
                 self.log_message.send_message(
                     'exception',
                     '{} has been deleted'.format(logfile['log_file'])
                 )
-                time.sleep(self.__daemon_sleep)
+                sleep(self.__daemon_sleep)
                 continue
 
             try:
                 if curr_inode != inode:
                     self.log_message.send_message(
                         'info',
-                        '%s has been rotated'.format(logfile['log_file'])
+                        '{} has been rotated'.format(logfile['log_file'])
                     )
                     inode = curr_inode
                     try:
@@ -194,13 +209,18 @@ class SumoLogic(object):
 
                     fp = open(logfile['log_file'], "r")
                     # this ultimately forces offset (if not 0) to be < last_offset
-                    last_offset = sys.maxsize
+                    last_offset = maxsize
 
-                offset = os.fstat(fp.fileno())[ST_SIZE]
+                offset = fstat(fp.fileno())[ST_SIZE]
                 if last_offset is None:
                     last_offset = offset
 
-                if offset > last_offset:
+                if offset == last_offset:
+                    self.log_message.send_message(
+                        'debug',
+                        '{} does not have any new data'.format(logfile['log_file'])
+                    )
+                elif offset > last_offset:
                     # new data added to logfile
                     self.log_message.send_message(
                         'debug',
@@ -225,7 +245,7 @@ class SumoLogic(object):
                     last_offset = 0
                     self.file_tracker.update_first_line()
                     continue
-                time.sleep(self.__daemon_sleep)
+                sleep(self.__daemon_sleep)
             except KeyboardInterrupt:
                 self.__lock_file.remove()
                 msg = "SumoLogic exited by user with Keyboard interrupt"
@@ -235,10 +255,10 @@ class SumoLogic(object):
     def process_log(self, logfile_info, offset):
         try:
             if logfile_info['log_file'].endswith(".gz"):
-                fp = gzip.open(logfile_info['log_file'], 'r')
+                fp = GzipFile(logfile_info['log_file'], 'r')
             elif logfile_info['log_file'].endswith(".bz2"):
                 if HAS_BZ2:
-                    fp = bz2.BZ2File(logfile_info['log_file'], 'r')
+                    fp = BZ2File(logfile_info['log_file'], 'r')
                 else:
                     raise Exception("Can not open bzip2 file (missing bz2 module)")
             else:
@@ -268,10 +288,12 @@ class SumoLogic(object):
 
         if self.sync.check_to_sync(self.__sync_interval):
             if self.sync.send_new_logdata(self.sync_lines, logfile_info):
+                mem_diff = memory_profiler.memory_usage()[0] - START_MEMORY[0]
                 self.sync_lines = []
+                self.log_message.send_message('info', '{} is using {} MB of memory'.format(self.__logfile['log_file'], mem_diff))
 
         return offset
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.log_message.send_message('info', 'Exiting Child Process for {}'.format(self.log_file['file_path']))
+        self.log_message.send_message('info', 'Exiting Child Process for {}'.format(self.__logfile['log_file']))
         self.__lock_file.remove()
